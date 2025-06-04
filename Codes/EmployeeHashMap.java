42f6fda4c4f0694821d0f702a63e4969789443a7
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-06-04 22:54:10 +0530

    Employee HashMap code

---
+++ Codes/EmployeeHashMap.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+// import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Set;
+
+public class EmployeeHashMap {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, Integer> employeeHashMap = new HashMap<>();
+
+        employeeHashMap.put(1001, 70000);
+        employeeHashMap.put(1002, 60000);
+        employeeHashMap.put(1003, 75000);
+        employeeHashMap.put(1004, 80000);
+        employeeHashMap.put(1005, 100000);
+
+        System.out.println("Hasmap of given data: " + employeeHashMap);
+
+        // Set<Integer> keys = employeeHashMap.keySet();
+        // System.out.println(keys);
+
+        System.out.println("The keys are: " + employeeHashMap.keySet());
+        System.out.println();
+
+        employeeHashMap.remove(001);
+        System.out.println("Hashmap after removing data: " + employeeHashMap);
+
+        employeeHashMap.replace(1003, 200000);
+        System.out.println("Map after updating: " + employeeHashMap);
+
+        System.out.println("Key present ? : " + employeeHashMap.containsKey(1009));
+
+        System.out.println("Value present in Hashmap :" + employeeHashMap.containsValue(200000));
+
+        System.out.println("The value corresponding to the key is: " + employeeHashMap.get(1003));
+
+        System.out.println(employeeHashMap.getOrDefault(1006, 50000));
+        // System.out.println(employeeHashMap);
+
+
+        // employeeHashMap.clear();
+        // System.out.println("The hashmap after clear: " + employeeHashMap);
+
+        System.out.println(employeeHashMap.size());
+
+        System.out.println(employeeHashMap.entrySet());
+
+
+        
+        Set<Integer> unique = new HashSet<>();
+
+       
+        
+        
+
+    }
+}
--- Codes/MeterSurgeDetection.java
+++ Codes/MeterSurgeDetection.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
-import java.util.*;
 
 public class MeterSurgeDetection {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter the number of meters: ");
         int n = scanner.nextInt();
         int [] inp = new int[n];
-        for (int i = 0; i < inp.length; i++) {
+        System.out.println("Enter the status codes(in number): ");
+        for (int i = 0; i < n; i++) {
             inp[i] = scanner.nextInt();
         }
-
+        scanner.close();
+        
+        int count = 0;
         for (int j = 0; j < inp.length; j++) {
-            String bin = Integer.toBinaryString(inp[j]);
+            int num = inp[j];
+            if((num &(1<<2)) != 0){
+                count++;
+            }
 
         }
-        
-        
+        System.out.println("Number of meters with tempering attempt: "+ count);
+
     }    
 }
